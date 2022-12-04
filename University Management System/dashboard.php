e8c4aa46fad28bd9a806fda46632a6801f5ccee0
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2022-12-04 23:38:47 +0530

    Dashboard

---
+++ University Management System/dashboard.php
@@ -0,0 +1,181 @@
+<!DOCTYPE HTML>
+<?php   
+session_start();  
+if(!isset($_SESSION["sess_user"])){  
+header("location:login.php");  
+} else {  
+?>  
+<html>
+  <head>
+    <title>University
+    </title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+  <body class="is-preload">
+    <!-- Wrapper -->
+    <div id="wrapper">
+      <!-- Main -->
+      <div id="main">
+        <div class="inner">
+          <!-- Header -->
+          <header id="header">
+            <a href="dashboard.php" class="logo">
+              <strong>University
+              </strong> Management System
+            </a>
+            <ul class="icons">
+              <li>
+                <a href="logout.php" class="icon">Logout
+                </a>
+              </li>
+            </ul>
+          </header>
+          <!-- Banner -->
+          <section id="banner">
+            <div class="content">
+              <header>
+                <h2>University
+                  <br/>
+                  Management System
+                </h2>
+                <p>
+                </p>
+              </header>
+              <p>A portal that allows you to do almost all the basic transactions of a university.
+              </p>
+              <ul class="actions">
+                <li>
+                  <a href="#" class="button big">Learn More
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <span class="image object">
+              <img src="images/background.jpg" alt="" />
+            </span>
+          </section>
+        </div>
+      </div>
+      <!-- Sidebar -->
+      <div id="sidebar">
+        <div class="inner">
+          <!-- Search -->
+          <section id="search" class="alt">
+            <form method="post" action="#">
+              <input type="text" name="query" id="query" placeholder="Search" />
+            </form>
+          </section>
+          <!-- Menu -->
+          <nav id="menu">
+            <header class="major">
+              <h2>Menu
+              </h2>
+            </header>
+            <ul>
+              <li>
+                <a href="dashboard.php">Homepage
+                </a>
+              </li>
+              <li>
+                <a href="View_Departments.php">Departments
+                </a>
+              </li>
+              <li>
+                <a href="View_Professors.php">Professors
+                </a>
+              </li>
+              <li>
+                <span class="opener">Students
+                </span>
+                <ul>
+                  <li>
+                    <a href="View_Students.php">View All
+                    </a>
+                  </li>
+                  <li>
+                    <a href="View_Students_G.php">Graduates
+                    </a>
+                  </li>
+                  <li>
+                    <a href="View_Students_U.php">Undergraduates
+                    </a>
+                  </li>
+                  <li>
+                    <a href="View_Students_N.php">Non Matriculating
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <span class="opener">Courses
+                </span>
+                <ul>
+                  <li>
+                    <a href="View_Courses.php">View Courses
+                    </a>
+                  </li>
+                  <li>
+                    <a href="View_Course_Sections.php">Course Sections
+                    </a>
+                  </li>
+                  <li>
+                    <a href="View_Lab_Session.php">Lab Sessions
+                    </a>
+                  </li>
+                </ul>
+              <li>
+                <span class="opener">Library
+                </span>
+                <ul>
+                  <li>
+                    <a href="View_Books.php">View Books
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Borrow_Book.php">Burrow Book
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Return_Book.php">Return Book
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <span class="opener">Company
+                </span>
+                <ul>
+                  <li>
+                    <a href="View_Company.php">View Companies
+                    </a>
+                  </li>
+                  <li>
+                    <a href="View_Company_Session.php">View Company Sessions
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          </nav>
+      </div>
+    </div>
+    </div>
+  <!-- Scripts -->
+  <script src="assets/js/jquery.min.js">
+  </script>
+  <script src="assets/js/browser.min.js">
+  </script>
+  <script src="assets/js/breakpoints.min.js">
+  </script>
+  <script src="assets/js/util.js">
+  </script>
+  <script src="assets/js/main.js">
+  </script>
+  </body>
+</html>
+<?php  
+}  
+?>  
